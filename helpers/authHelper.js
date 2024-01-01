651d08114c40b893f2da5ff3477261fe334c9995
Germán Alberto Valencia Torres <lenovopromotor@gmail.com>
2024-01-01 12:34:30 -0500

    first user

--- helpers/authHelper.js
+++ helpers/authHelper.js
@@ -1,9 +1,10 @@
 import bcrypt from "bcrypt";
 
+const SALT_ROUNDS = 10;
+
 export const hashPassword = async (password) => {
   try {
-    const saltRounds = 10;
-    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
     return hashedPassword;
   } catch (error) {
     console.log(error);
